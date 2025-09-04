012594b801d8d3a2f90aa13096780c4f32014dcb
ericayala412 <69561611+ericayala412@users.noreply.github.com>
2025-09-03 20:27:44 -0400

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,6 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
   },
