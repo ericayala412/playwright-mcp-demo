e99b70ff55a359caf641f9841206edb6597097c8
ericayala412 <69561611+ericayala412@users.noreply.github.com>
2025-09-03 20:45:38 -0400

    Update playwright.config.ts
    Removed firefox

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,9 +20,5 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
   ],
 });
